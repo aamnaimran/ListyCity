362061e5f8ad1409ae6a3bd2693788873bd2f617
aamnaimran <aamnaimran588@gmail.com>
2026-02-03 13:53:23 +0500

    Updated delete function and resolve null deletecitybutton pointer exception

--- app/src/main/java/com/example/listycity/MainActivity.java
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.AdapterView;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,8 +26,7 @@ public class MainActivity extends AppCompatActivity {
     EditText addCityField;
     Button addCityButton;
     Button deleteCityButton;
-    int selectedPosition = -1;
-
+    private int selectedPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,16 +56,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //delete cities; long click
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedPosition = position; // get position of city to delete
+            Toast.makeText(this, "Selected: " + dataList.get(position), Toast.LENGTH_SHORT).show(); //confirmation message
+        });
+
         deleteCityButton.setOnClickListener(v -> {
             if (selectedPosition != -1 && selectedPosition < dataList.size()) {
                 dataList.remove(selectedPosition);
-                cityAdapter.notifyDataSetChanged();
-                selectedPosition = -1; // Reset selection
+                cityAdapter.notifyDataSetChanged(); // refresh UI so change is reflected
+                selectedPosition = -1; // reset selection variable
             }
         });
-
-
+//
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
